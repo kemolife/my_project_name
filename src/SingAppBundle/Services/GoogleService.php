9862a8b56924e80f6c313f31f08b3fa3b010a6be
vitalii <kemolife1990@gmail.com>
2018-07-13 14:18:26 +0300

    change

--- src/SingAppBundle/Services/GoogleService.php
+++ src/SingAppBundle/Services/GoogleService.php
@@ -23,7 +23,7 @@ class GoogleService
     public function auth()
     {
         $client = new Google_Client();
-        $client->setAuthConfig('client_secret_389043269771-eqgracqvstr97pmks1nkqhj0reu50pf3.apps.googleusercontent.com.json');
+        $client->setAuthConfig('client_secret.json');
         $client->setAccessType("offline");        // offline access
         $client->setIncludeGrantedScopes(true);   // incremental auth
         $client->setScopes(['https://www.googleapis.com/auth/plus.business.manage']);
@@ -37,7 +37,7 @@ class GoogleService
     {
 
         $client = new Google_Client();
-        $client->setAuthConfig('client_secret_389043269771-eqgracqvstr97pmks1nkqhj0reu50pf3.apps.googleusercontent.com.json');
+        $client->setAuthConfig('client_secret.json');
         $client->setAccessType("offline");        // offline access
         $client->setIncludeGrantedScopes(true);   // incremental auth
         $client->setScopes(['https://www.googleapis.com/auth/plus.business.manage']);
@@ -49,7 +49,7 @@ class GoogleService
     public function getLocations(GoogleAccount $googleAccount)
     {
         $client = new Google_Client();
-        $client->setAuthConfig('client_secret_389043269771-eqgracqvstr97pmks1nkqhj0reu50pf3.apps.googleusercontent.com.json');
+        $client->setAuthConfig('client_secret.json');
         $client->setAccessToken($googleAccount->getAccessToken());
         $client->setAccessType('offline');
         $client->setIncludeGrantedScopes(true);   // incremental auth
