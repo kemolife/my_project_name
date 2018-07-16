96f45bbf342dd4f3cefaf52eb18d252a34eba09a
vitalii <kemolife1990@gmail.com>
2018-07-16 10:41:16 +0300

    listings migration #2

--- src/SingAppBundle/Security/FormAuthenticator.php
+++ src/SingAppBundle/Security/FormAuthenticator.php
@@ -92,7 +92,7 @@ class FormAuthenticator extends AbstractGuardAuthenticator
      */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        return new RedirectResponse($this->router->generate('business-add'));
+        return new RedirectResponse($this->router->generate('add-business'));
     }
 
     /**
