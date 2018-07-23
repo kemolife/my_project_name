bbb3f19feee23cb15ae0f8a9326cc67c717699a5
vitalii <kemolife1990@gmail.com>
2018-07-23 17:48:03 +0300

    change

--- src/SingAppBundle/Services/PinterestService.php
+++ src/SingAppBundle/Services/PinterestService.php
@@ -35,7 +35,7 @@ class PinterestService
 
     public function createAccount(Response $accessTokeData)
     {
-        var_dump($accessTokeData); die;
+        var_dump($accessTokeData->access_token); die;
         $createdDate = new \DateTime();
         $pinterest = new PinterestAccount();
 
