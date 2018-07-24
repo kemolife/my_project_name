c62b8f339aa2cc90855b9cff18f029c8976f9e09
vitalii <kemolife1990@gmail.com>
2018-07-24 16:20:15 +0300

    Change

--- src/SingAppBundle/Controller/BingController.php
+++ src/SingAppBundle/Controller/BingController.php
@@ -68,6 +68,6 @@ class BingController extends BaseController
          */
         $bingService = $this->get('app.bing.service');
         $account = $bingService->getBingAccount($user, $business);
-        var_dump($bingService->getOwner($account->getAccessToken())); die;
+        var_dump($bingService->setTokenObject($account)->getOwner()); die;
     }
 }
