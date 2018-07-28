c5af7d0de7ee611747bd84f345cfe290306cc716
vitalii <kemolife1990@gmail.com>
2018-07-28 18:57:40 +0300

    xx

--- app/DoctrineMigrations/Version20180728155612.php
+++ app/DoctrineMigrations/Version20180728155612.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180728144531 extends AbstractMigration
+final class Version20180728155612 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
