71d264fdc8011db2b87dfebdb5dd33ef659d6d6e
vitalii <kemolife1990@gmail.com>
2018-07-30 11:28:55 +0300

    Change

--- src/SingAppBundle/Services/ZomatoService.php
+++ src/SingAppBundle/Services/ZomatoService.php
@@ -47,7 +47,9 @@ class ZomatoService
         if ($zomatoAccount instanceof ZomatoAccount) {
             $this->curl->setOpt(CURLOPT_SSL_VERIFYPEER, 1);
             $this->curl->setOpt(CURLOPT_SSL_VERIFYPEER, 2);
-            $this->curl->setOpt(CURLOPT_CAINFO, $this->webDir .  "/cacert-2017-09-20.pem");
+            $this->curl->setOpt(CURLOPT_CAINFO, $this->webDir .  "/cacert-2018-06-20.pem");
+            $this->curl->get('https://www.zomato.com/pl/perth');
+            var_dump($this->curl->response); die;
             $this->curl->setHeaders([
                 'cookie' => $this->getCookies()
             ]);
