92a46fc9126384b6fb70e4d838b41bf5c2fd6804
vitalii <kemolife1990@gmail.com>
2018-08-14 11:39:26 +0300

    change services posting

--- src/SingAppBundle/Services/PinterestService.php
+++ src/SingAppBundle/Services/PinterestService.php
@@ -67,9 +67,10 @@ class PinterestService
         foreach ($this->getPins($pinterestAccount->getAccessToken()) as $pin){
             $pinterestPin = new PinterestPin();
             $pinterestPin->setLink($pin->url);
+            $pinterestPin->setTitle($pin->url);
             $pinterestPin->setBoard($pin->board);
             $pinterestPin->setCaption($pin->note);
-            $pinterestPin->setPostDate(new \DateTime($pin->created_at));
+            $pinterestPin->setPostDate($pin->created_at);
             $pinterestPin->setSocialNetwork('pinterest');
             $pinterestPin->setBusiness($pinterestAccount->getBusiness());
             $this->em->persist($pinterestPin);
