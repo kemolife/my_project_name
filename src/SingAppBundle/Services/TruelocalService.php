f04805e1e6820da11fc2cde009fac628f006adbb
vitalii <kemolife1990@gmail.com>
2018-08-15 16:56:50 +0300

    change services posting

--- src/SingAppBundle/Services/TruelocalService.php
+++ src/SingAppBundle/Services/TruelocalService.php
@@ -159,7 +159,6 @@ class TruelocalService implements BaseInterface, ScraperInterface, CreateService
             $params['description'] = $business->getDescription();
             $params['phoneNumber'] = '0'.trim($business->getPhoneNumber(), '+61');
             $params['hideSuburb'] = false;
-            var_dump( $params); die;
             $this->curl->post('https://api.truelocal.com.au/rest/users/'.$truelocalAccount->getProfile().'/update?passToken=' . $cookies['token'], json_encode($params));
             if ($this->curl->response->meta->httpCode === 400) {
                 throw new OAuthCompanyException(json_encode($this->curl->response->meta->errors));
